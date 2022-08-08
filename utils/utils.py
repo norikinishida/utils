1f83f4ade27f06348d4644c2d2517ebf192fdb9d
norikinishida <norikinishida@gmail.com>
2022-08-08 18:05:45 +0900

    Add WhitespaceTokenizer

--- utils/utils.py
+++ utils/utils.py
@@ -12,10 +12,13 @@ import sys
 import time
 
 import numpy as np
+import spacy
+from spacy.tokens import Doc
 import pandas as pd
 import pyhocon
 import pyprind
 
+​
 
 ###############################
 # Logging
@@ -1551,6 +1554,29 @@ def normalize_string(string, able=None):
         string = re.sub(r"&gt;", ">", string)
     return string
 
+
+class WhitespaceTokenizer:
+    def __init__(self, vocab):
+        self.vocab = vocab
+​
+    # def __call__(self, text):
+        # words = text.split(" ")
+    def __call__(self, words):
+        spaces = [True] * len(words)
+        # Avoid zero-length tokens
+        for i, word in enumerate(words):
+            if word == "":
+                words[i] = " "
+                spaces[i] = False
+        # Remove the final trailing space
+        if words[-1] == " ":
+            words = words[0:-1]
+            spaces = spaces[0:-1]
+        else:
+           spaces[-1] = False
+​
+        return Doc(self.vocab, words=words, spaces=spaces)
+​
 
 def get_random_english_word():
     """
