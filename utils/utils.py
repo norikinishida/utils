a037eea28d25bb252e95774a8961a94411cdfd71
norikinishida <norikinishida@gmail.com>
2024-04-22 03:18:57 +0900

    minor update

--- utils/utils.py
+++ utils/utils.py
@@ -26,9 +26,11 @@ from tqdm import tqdm
 ###############################
 
 
-logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
-                    datefmt="%m/%d/%Y %H:%M:%S",
-                    level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    # datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO
+)
 logger = logging.getLogger()
 
 
